2c68f2fe4eb71be1593618d993d5d61d5a38559b
suhejbmusliu <suhejbmusliu69@gmail.com>
2024-01-02 17:46:21 +0100

    change

--- script.js
+++ script.js
@@ -9,21 +9,17 @@ let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
 
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 
-
 carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
     carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
 });
-
 
 carouselChildrens.slice(0, cardPerView).forEach(card => {
     carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 });
 
-
 carousel.classList.add("no-transition");
 carousel.scrollLeft = carousel.offsetWidth;
 carousel.classList.remove("no-transition");
-
 
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -64,7 +60,6 @@ const infiniteScroll = () => {
         carousel.classList.remove("no-transition");
     }
 
-   
     clearTimeout(timeoutId);
     if(!wrapper.matches(":hover")) autoPlay();
 }
@@ -74,14 +69,12 @@ const autoPlay = () => {
     timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
 }
 
-
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
-
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -94,15 +87,20 @@ window.onscroll = () => {
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
+        
+            if (navLinks.length > 0) {
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    const targetLink = document.querySelector('header nav a[href*=' + id + ']');
+                    if (targetLink) {
+                        targetLink.classList.add('active');
+                    }
+                });
+            }
         };
     });
 };
 
-// Add smooth scroll behavior
 navLinks.forEach(link => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
@@ -114,5 +112,25 @@ navLinks.forEach(link => {
             top: targetElement.offsetTop - 150,
             behavior: 'smooth'
         });
+
+        navBar.classList.remove("active");
     });
 });
+
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function(){
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
+
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    var scrollPosition = window.scrollY;
+  
+    if (scrollPosition > 80) {
+      header.classList.add("fixed-header");
+    } else {
+      header.classList.remove("fixed-header");
+    }
+  });
+    
